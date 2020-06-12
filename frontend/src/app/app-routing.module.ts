2cbddf2e67c72246c4ac56c209f36a9bd27bb80d
Michelle Broens <michellebroens96@gmail.com>
2020-06-12 16:21:56 +0200

    Removed unused component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UserComponent } from "./components/user/user.component";
 import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
 import { LobbyComponent } from "./components/lobby/lobby.component";
 import { RegisterComponent } from "./components/register/register.component";
